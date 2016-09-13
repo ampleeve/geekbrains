55704db86bc8c381c0595ca4753a32040dea651f
ampleeve <e@ampleev.com>
2016-09-13 19:48:22 +0300

    fix (reverted from commit 02c9c22b262a8b495a653838288c53aeeed37098)

--- index.php
+++ index.php
@@ -1,37 +1,4 @@
-<!DOCTYPE html>
-<html>
-    
-    <head>
-        <meta charset="utf8">
-        <title>Мой первый блог</title>
-        <link rel="stylesheet" href="style.css">
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    </head>
-   
-    <body>
-        <div class="container">
-        <h1>Мой первый блог</h1>
-            
-            <div class="article">
-            
-            <h3>
-                Заголовок h3
-            </h3>
-                
-            <em>Опубликовано: [дата публикации] </em>
-                
-            <p>
-                [содержание статьи]
-            </p>
-            
-            </div>
-          
-        </div>
-        
-        <footer>
-        <p>Мой первый блог <br> Copyright &copy; 2016</p>
-        </footer>
-    </body>
+<?php
 
-</html>
+echo "lesson number one"
+?>
