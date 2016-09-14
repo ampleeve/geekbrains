df65e75714442d290f2aa0053b038bfaedb81895
ampleeve <e@ampleev.com>
2016-09-14 19:20:46 +0300

    вывод от препода булевых и xor

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-phpinfo();
+//phpinfo();
+
+$a1=true;
+$a2=false;
+//Это исключающее или. TRUE если $a, или $a1 TRUE, но не оба.
+var_dump($a1 xor $a1);//false
+
+var_dump($a1 xor $a2);//true
+var_dump($a2 xor $a1);//true
+var_dump($a2 xor $a2);//false
+
 ?>
