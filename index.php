1cc0ee17b7743fc469bad0066bb5b83ccbdc1fd4
ampleeve <e@ampleev.com>
2016-09-21 18:12:37 +0300

    fix switch case

--- index.php
+++ index.php
@@ -29,22 +29,22 @@ $a = mt_rand(0,15);
 echo $a;
 echo '<br>';
 switch($a){
-    case 0: echo '0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15'; break;
-    case 1: echo '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15'; break;
-    case 2: echo '2,3,4,5,6,7,8,9,10,11,12,13,14,15'; break;
-    case 3: echo '3,4,5,6,7,8,9,10,11,12,13,14,15'; break;
-    case 4: echo '4,5,6,7,8,9,10,11,12,13,14,15'; break;
-    case 5: echo '5,6,7,8,9,10,11,12,13,14,15'; break;
-    case 6: echo '6,7,8,9,10,11,12,13,14,15'; break;
-    case 7: echo '7,8,9,10,11,12,13,14,15'; break;
-    case 8: echo '8,9,10,11,12,13,14,15'; break;
-    case 9: echo '9,10,11,12,13,14,15'; break;
-    case 10: echo '10,11,12,13,14,15'; break;
-    case 11: echo '11,12,13,14,15'; break;
-    case 12: echo '12,13,14,15'; break;
-    case 13: echo '13,14,15'; break;
-    case 14: echo '14,15'; break;
-    case 15: echo '15'; break;
+    case 0: echo '0'.'<br>';
+    case 1: echo '1'.'<br>';
+    case 2: echo '2'.'<br>';
+    case 3: echo '3'.'<br>';
+    case 4: echo '4'.'<br>';
+    case 5: echo '5'.'<br>';
+    case 6: echo '6'.'<br>';
+    case 7: echo '7'.'<br>';
+    case 8: echo '8'.'<br>';
+    case 9: echo '9'.'<br>';
+    case 10: echo '10'.'<br>';
+    case 11: echo '11'.'<br>';
+    case 12: echo '12'.'<br>';
+    case 13: echo '13'.'<br>';
+    case 14: echo '14'.'<br>';
+    case 15: echo '15'.'<br>';
 }
 
 /*
