2c584fc8f8f6a6b3ffeee6207a63b0069c485832
ampleeve <e@ampleev.com>
2016-09-26 23:06:38 +0300

    fix

--- index.php
+++ index.php
@@ -2,8 +2,47 @@
 <html>
 <head>
   <meta charset="UTF-8">
-  <title>test</title>
+  <title></title>
 </head>
 <body>
+  <?php
+
+  $goods = [
+
+    [
+        'id' => 1,
+        'name' => 'HTC',
+        'desc' => 'Описание для HTC',
+        'img' => 'htc.jpg',
+        'price' => 10000
+
+    ],
+
+    [
+        'id' => 2,
+        'name' => 'iPhone',
+        'desc' => 'Описание для iPhone',
+        'img' => 'iphone.jpg',
+        'price' => 50000
+
+    ],
+
+    [
+        'id' => 3,
+        'name' => 'SAMSUNG',
+        'desc' => 'Описание для SUMSUNG',
+        'img' => 'samsung.jpg',
+        'price' => 25000
+
+    ],
+
+
+  ];
+echo '<pre>';
+  foreach ($goods as $good){
+    var_dump($good);
+  }
+
+  ?>
 </body>
 </html>
