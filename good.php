45bdaea9f9141d2e6ab60cc95ad2f7d1c8be43ff
ampleeve <e@ampleev.com>
2016-09-28 13:37:54 +0300

    fix

--- good.php
+++
@@ -1,4 +0,0 @@
-<h1><?php echo $selectedGood['name']?></h1>
-<img src="goods/<?= $selectedGood['img']; ?>" style = "width: 500px;">
-<p><?=  $selectedGood['desc']; ?></p>
-<p><?= $selectedGood['price']. " руб.";?></p>
