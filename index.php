f90b5059bceec5bd6e55c47c4f9d752909f733d0
ampleeve <e@ampleev.com>
2016-09-29 18:14:35 +0300

    ыва

--- index.php
+++ index.php
@@ -1,17 +1,28 @@
 <?php
 
-session_start();
-$_SESSION['user'] = 'Vasily';
-//unset($_SESSION['user']);
-session_destroy();
-echo '<pre>';
-var_dump($_SESSION['user']);
+<?php   // блок для описания функций, используемых в задании к 4 уроку
 
-/*setcookie('name', 'Vasiliy', time() - 3600 * 24);
-if (isset($_COOKIE ['name'])) {
-    echo $_COOKIE ['name'];
-}else{
-    echo 'Пусто';
+/**
+ * @param $nameSelect
+ * @param $operation
+ * @return string
+ */
+function checkSelectChange($nameSelect, $operation)
+
+        // Проврка - передавалось ли методом Пост $nameSelect и ровно ли оно $operation.
+        // Если да, то надо селект по умолчанию выставить в $operation
+
+{
+    return isset($_POST[$nameSelect]) && $_POST[$nameSelect] == $operation ? "selected='selected'" : '';
+}
+
+function checkArgInput($name)
+
+        //Проверка - передавался ли в пост $name. Если да, то вернуть его значение как параметр value
+
+{
+    return isset($_POST[$name]) ? "value=\"$_POST[$name]\"" : '';
+>>>>>>> Stashed changes
 }
 ?>
 
