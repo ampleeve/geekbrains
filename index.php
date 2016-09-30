44486c88d4079eb8baefe0e97205e764beb0684d
ampleeve <e@ampleev.com>
2016-09-30 15:19:42 +0300

    dfsdf

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once ('functions.php');
 <p>Привет, <?= getCurrentUserName(); ?></p>
 
 <?php if (isAuthorized()): ?>
-    <a href="Logout.php">Выйти</a>
+    <a href="logout.php">Выйти</a>
     <?php else: ?>
-    <a href="Login.php">Войти</a>
+    <a href="login.php">Войти</a>
 <?php endif;?>
