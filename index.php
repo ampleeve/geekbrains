7b1abe5cd601641a6c179fd801dcee54d394f82f
ampleeve <e@ampleev.com>
2016-09-30 13:23:55 +0300

    sdf

--- index.php
+++ index.php
@@ -1,22 +1,7 @@
 <?php
-session_start();
-require_once ('functions.php');
-?>
-
-<p>Привет, <?= getCurrentUserName(); ?></p>
-
-<?php if (isAuthorized()): ?>
-    <a href="Logout.php">Выйти</a>
-<?php else: ?>
-    <a href="Login.php">Войти</a>
-<?php endif;?>
-
-<?php
-if (isAuthorized()){
-    redirect($_SESSION['lastPage']);
-}else{
-    redirect('Login.php');
-}
-?>
-
-
+/**
+ * Created by PhpStorm.
+ * User: evgenijampleev
+ * Date: 30.09.16
+ * Time: 13:23
+ */
--- pageA.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();
-echo 'pageA';
-$_SESSION['lastPage'] = 'pageA';
