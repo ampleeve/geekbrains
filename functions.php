979176868181fe2bc1c32797d02120fef2c26656
ampleeve <e@ampleev.com>
2016-09-30 13:40:44 +0300

    sdf

--- functions.php
+++ functions.php
@@ -1,1 +1,17 @@
 <?php
+function getAllUsers(){
+    return [
+        [
+            'id' => 1,
+            'login' => 'admin',
+            'password' => md5('123')
+
+        ],
+        [
+            'id' => 2,
+            'login' => 'user',
+            'password' => md5('456')
+
+        ]
+    ];
+}
