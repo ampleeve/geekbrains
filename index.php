2c914706a0e84c754d8b87a0479664f2abdf535f
ampleeve <e@ampleev.com>
2016-10-04 16:43:10 +0300

    разминка с файлами

--- index.php
+++ index.php
@@ -1,52 +1,59 @@
 <?php
 
-$f = fopen('test.txt', 'r');
-$c = fread($f, 1);
-while ($c != null){
-   // echo "$c <br>";
-    $c = fread($f, 1);
-}
-/*
-function log($msg){
-    $time = date('H:i:s');
-    $f = fopen('log.txt', 'a+');
-    fputs($f, "$time: $msg \n");
-    fclose($f);
-}
 
-if($f){
-    log('$f - истина');
-}else{
-    log('$f - ложь');
+function uploadFile($file){
+    if($file['name'] == ''){
+        echo "Файл не выбран!";
+    }
+
+    if(copy($file['tmp_name'], 'img/'.$file['name'])){
+        echo "Файл успешно загружен";
+    }
+    else{
+        echo "Ошибка загрузки файла";
+    }
 }
 
-function log($msg) {
-    $time = date('H:i:s');
-    $f = fopen('log.txt', 'a+'); fputs($f, "$time: $msg \n"); fclose($f);
-}
-if($c){ log('попали сюда');
-} else{
-    log('попали туда');
-}*/
 
 
-/*
-$file = 'test.txt';
-$data = 'Привет!';
-file_put_contents($file, $data);
-echo file_get_contents($file);
-*/
-
+/*$var = mt_rand(0,1);
 
 function log1($msg) {
-    $time = date('H:i:s');
-    $stream = fopen('log.txt', 'a+');
-    fputs($stream, "$time: $msg \n");
-    fclose($stream);
+$time = date('H:i:s');
+$f = fopen('log.txt', 'a+'); fputs($f, "$time: $msg \n"); fclose($f);
 }
+if($var==1){
 
-log1('test');
-/*if($c){ log('попали сюда');
-} else{
-    log('попали туда');
-}*/
+    log1('попали сюда');
+
+} else {
+
+    log1('попали туда');
+
+}
+echo '<pre>';
+$var = file('log.txt');
+var_dump($var);*/
+?>
+<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8"/>
+            <title>Загрузка файлов на сервер</title>
+        </head>
+        <body>
+        <h1>Пример загрузки файлов на сервер</h1>
+
+        <?php
+        if(isset($_FILES['file'])){
+            uploadFile($_FILES['file']);
+        }
+        ?>
+
+        <form method="POST" enctype="multipart/form-data">
+            <input type="file" name="file" />
+            <input type="submit" value = 'Загрузить файл'>
+        </form>
+
+        </body>
+    </html>
