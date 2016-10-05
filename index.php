46b3c378bd49b9d4948cbbf8fd87539b31c2217e
ampleeve <e@ampleev.com>
2016-10-05 14:42:05 +0300

    sdf

--- index.php
+++ index.php
@@ -1,51 +1,39 @@
 <?php
 
-function showImagesFromDir($path){
+require_once ('functions.php');
+phpinfo();
 
-    $images = scandir($path);
-    foreach ($images as $image){
-      //  echo getimagesize($path.$image['tmp_name']);
-
-    }
-
-}
-function uploadFile($file){
-
-    if($file['name'] == '' || !getimagesize($file['tmp_name']) ){
-        echo "Файл не выбран или вы пытаетесь загрузить не изображение, к сожалению..";
-    }
-    elseif(copy($file['tmp_name'], 'img/'.$file['name'])){
-        echo "Файл успешно загружен";
-    }
-    else{
-        echo "Ошибка загрузки файла";
-    }
+if($_SERVER['REQUEST_METHOD']==='POST' && !empty($_FILES['image'])){
+    $error = handleRequest($_FILES['image']);
 }
 ?>
+
+
 <!DOCTYPE html>
     <html>
+
         <head>
             <meta charset="utf-8"/>
             <title>Фотогалерея</title>
         </head>
+
         <body>
-        <h1>Фотогалерея</h1>
+            <h1>Фотогалерея</h1>
 
-        <?php
-            showImagesFromDir('img/');
-        ?>
+            <div>
+            <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST" enctype="multipart/form-data">
+                <label for="loadImage">Загрузить новую картинку:</label>
+                <input id="loadImage" type="file" name="image" />
+                <input type="submit" value = 'Загрузить изображение'>
+            </form>
+            </div>
 
-        <?php
-        if(isset($_FILES['image'])){
-            uploadFile($_FILES['image']);
-        }
-        ?>
-        <img src="/Applications/XAMPP/htdocs/geekbrains.git/img/3bIeUw6Hl9s.jpg">
-
-        <form action="<?= $_SERVER['PHP_SELF'] ?>" method="POST" enctype="multipart/form-data">
-            <input type="file" name="image" />
-            <input type="submit" value = 'Загрузить изображение'>
-        </form>
-
+            <?php foreach (getImages() as $image): ?>
+            <div>
+                <a href = "img.php?img= <?= $image ?>">
+                    <img src = "img/min/<?= $image ?>">
+                </a>
+            </div>
+            <?php endforeach; ?>
         </body>
     </html>
