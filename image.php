bf684ff113dc36d9ca583748daacc39dd8e7e388
ampleeve <e@ampleev.com>
2016-10-16 09:03:07 +0300

    sda

---
+++ image.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Галерея</title>
+        </head>
+        <body>
+        <a href="index.php">Назад</a>
+        <img src="img/full/<?=$_GET['img'] ?>" style="...">
+        </body>
+    </html>
