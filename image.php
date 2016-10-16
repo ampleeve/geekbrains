80a7aa5c12d21b047e9ef964269ae01eda4860d7
ampleeve <e@ampleev.com>
2016-10-16 13:09:08 +0300

    sdf

---
+++ image.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Галерея</title>
+        </head>
+        <body>
+        <a href="index.php">Назад</a>
+        <br>
+        <img src="img/full/<?=$_GET['img'] ?>" style="...">
+        </body>
+    </html>
