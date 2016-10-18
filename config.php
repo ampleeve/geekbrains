94c779f74b0dbdc3d1e76886d8dd82377b535974
ampleeve <e@ampleev.com>
2016-10-18 16:47:51 +0300

    fsdf

--- config.php
+++ config.php
@@ -8,9 +8,9 @@ define('VIEWS_DIR', SITE_ROOT . '/views');
 define('CONTROLLERS_DIR', SITE_ROOT . '/controllers');
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'blog');
+define('DB_USER', 'ampleeve');
+define('DB_PASS', 'password');
+define('DB_NAME', 'test_db');
 
 require_once (LIB_DIR . '/core.php');
 require_once (LIB_DIR . '/db.php');
